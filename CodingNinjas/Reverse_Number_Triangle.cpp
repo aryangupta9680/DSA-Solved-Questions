18e1dbe9ed93271dc1e5269422bbd5932b699c6e
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-02-23 08:39:26 +0530

    Add solution for Reverse Number Triangle problem

---
+++ CodingNinjas/Reverse_Number_Triangle.cpp
@@ -0,0 +1,13 @@
+void nNumberTriangle(int n)
+{
+    int row, col;
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= n - (row - 1); col++)
+        {
+            cout << col << " ";
+        }
+
+        cout << endl;
+    }
+}
