a14031abd4174dffc79341634bbb4360a2b1ad46
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-03-21 00:37:41 +0530

    Add solution for Insertion Sort problem

---
+++ CodingNinjas/Insertion_Sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+void insertionSort(int n, vector<int> &arr)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
