9cbcff390f1b8c7bd22173cd999b9fb9e0c2db19
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-04-21 13:51:37 +0530

    Add solution for Symmetry problem

---
+++ CodingNinjas/Symmetry.cpp
@@ -0,0 +1,44 @@
+void symmetry(int n)
+{
+    int row, col;
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        for (col = 1; col <= (2 * n - 2 * row); col++)
+        {
+            cout << "  ";
+        }
+
+        for (col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for (row = n - 1; row >= 1; row--)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        for (col = 1; col <= (2 * n - 2 * row); col++)
+        {
+            cout << "  ";
+        }
+
+        for (col = 1; col <= row; col++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
