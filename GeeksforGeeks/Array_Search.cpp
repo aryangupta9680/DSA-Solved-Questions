edbd93eadb4c95f591d7b1df5adb02d21254651e
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-05-13 10:36:21 +0530

    Add second method to solve Array Search problem

--- GeeksforGeeks/Array_Search.cpp
+++ GeeksforGeeks/Array_Search.cpp
@@ -1,3 +1,4 @@
+// First Method:
 class Solution
 {
 public:
@@ -15,5 +16,35 @@ public:
         }
 
         return -1;
+    }
+};
+
+// Second Method:
+class Solution
+{
+public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int linearsearch(vector<int> &arr, int X, int index, int N)
+    {
+        // base condition
+        if (index == N)
+        {
+            return -1;
+        }
+
+        if (arr[index] == X)
+        {
+            return index;
+        }
+
+        return linearsearch(arr, X, index + 1, N);
+    }
+
+    int search(vector<int> &arr, int X)
+    {
+        int N = arr.size();
+        return linearsearch(arr, X, 0, N);
     }
 };
