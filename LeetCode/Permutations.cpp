ddf83981085feda7a31c0665cdaa7f0226bddcd8
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-06-07 13:16:27 +0530

    Add second method

--- LeetCode/Permutations.cpp
+++ LeetCode/Permutations.cpp
@@ -1,34 +1,65 @@
+// First Method:
+
+// class Solution
+// {
+// public:
+//     void permutation(vector<int> &arr, vector<vector<int>> &ans, vector<int> &temp, vector<bool> &visited)
+//     {
+//         // base condition
+//         if (visited.size() == temp.size())
+//         {
+//             ans.push_back(temp);
+//             return;
+//         }
+
+//         for (int i = 0; i < visited.size(); i++)
+//         {
+//             if (visited[i] == 0)
+//             {
+//                 visited[i] = 1;
+//                 temp.push_back(arr[i]);
+//                 permutation(arr, ans, temp, visited);
+//                 visited[i] = 0;
+//                 temp.pop_back();
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int> &nums)
+//     {
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         vector<bool> visited(nums.size(), 0);
+//         permutation(nums, ans, temp, visited);
+//         return ans;
+//     }
+// };
+
+// Second Method:
 class Solution
 {
 public:
-    void permutation(vector<int> &arr, vector<vector<int>> &ans, vector<int> &temp, vector<bool> &visited)
+    void permutation(vector<int> &arr, vector<vector<int>> &ans, int index)
     {
         // base condition
-        if (visited.size() == temp.size())
+        if (arr.size() == index)
         {
-            ans.push_back(temp);
+            ans.push_back(arr);
             return;
         }
 
-        for (int i = 0; i < visited.size(); i++)
+        for (int i = index; i < arr.size(); i++)
         {
-            if (visited[i] == 0)
-            {
-                visited[i] = 1;
-                temp.push_back(arr[i]);
-                permutation(arr, ans, temp, visited);
-                visited[i] = 0;
-                temp.pop_back();
-            }
+            swap(arr[i], arr[index]);
+            permutation(arr, ans, index + 1);
+            swap(arr[i], arr[index]);
         }
     }
 
     vector<vector<int>> permute(vector<int> &nums)
     {
         vector<vector<int>> ans;
-        vector<int> temp;
-        vector<bool> visited(nums.size(), 0);
-        permutation(nums, ans, temp, visited);
+        permutation(nums, ans, 0);
         return ans;
     }
 };
