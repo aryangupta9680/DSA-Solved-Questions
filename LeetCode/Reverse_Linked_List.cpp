44d335ad4cf451f59b3c58d545da96945f08eb9f
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-06-25 10:28:54 +0530

    Add second method

--- LeetCode/Reverse_Linked_List.cpp
+++ LeetCode/Reverse_Linked_List.cpp
@@ -1,3 +1,4 @@
+// First Method:
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -30,6 +31,35 @@ public:
             temp = temp->next;
         }
 
+        return head;
+    }
+};
+
+// Second Method:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *curr = head, *prev = NULL, *fut = NULL;
+        while (curr)
+        {
+            fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
+        }
+        head = prev;
         return head;
     }
 };
