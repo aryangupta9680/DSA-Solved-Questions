a2b0ff088ab7171de2b411427b402467634412dc
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-07-02 23:52:22 +0530

    Update solution for Flattening a Linked List problem

--- GeeksforGeeks/Flattening_a_Linked_List.cpp
+++ GeeksforGeeks/Flattening_a_Linked_List.cpp
@@ -14,45 +14,45 @@ struct Node{
 };
 */
 
-Node *merge(Node *head1, Node *head2)
-{
-    Node *head = new Node(0);
-    Node *tail = head;
-    while (head1 && head2)
-    {
-        if (head1->data <= head2->data)
-        {
-            tail->bottom = head1;
-            head1 = head1->bottom;
-            tail = tail->bottom;
-            tail->bottom = NULL;
-        }
-
-        else
-        {
-            tail->bottom = head2;
-            head2 = head2->bottom;
-            tail = tail->bottom;
-            tail->bottom = NULL;
-        }
-    }
-
-    if (head1)
-    {
-        tail->bottom = head1;
-    }
-    else
-    {
-        tail->bottom = head2;
-    }
-
-    return head->bottom;
-}
-
 class Solution
 {
 public:
     // Function which returns the  root of the flattened linked list.
+    Node *merge(Node *head1, Node *head2)
+    {
+        Node *head = new Node(0);
+        Node *tail = head;
+        while (head1 && head2)
+        {
+            if (head1->data <= head2->data)
+            {
+                tail->bottom = head1;
+                head1 = head1->bottom;
+                tail = tail->bottom;
+                tail->bottom = NULL;
+            }
+
+            else
+            {
+                tail->bottom = head2;
+                head2 = head2->bottom;
+                tail = tail->bottom;
+                tail->bottom = NULL;
+            }
+        }
+
+        if (head1)
+        {
+            tail->bottom = head1;
+        }
+        else
+        {
+            tail->bottom = head2;
+        }
+
+        return head->bottom;
+    }
+
     Node *flatten(Node *root)
     {
         Node *head1, *head2, *head3;
