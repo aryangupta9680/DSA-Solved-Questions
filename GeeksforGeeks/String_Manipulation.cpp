cbf7683a791cca4bcf963760771b9ac1787d85e2
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-07-06 18:38:39 +0530

    Add solution for String Manipulation problem

---
+++ GeeksforGeeks/String_Manipulation.cpp
@@ -0,0 +1,27 @@
+// User function Template for C++
+class Solution
+{
+public:
+    int removeConsecutiveSame(vector<string> &arr)
+    {
+        stack<string> s;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (s.empty())
+            {
+                s.push(arr[i]);
+            }
+            else if (s.top() == arr[i])
+            {
+                s.pop();
+            }
+            else
+            {
+                s.push(arr[i]);
+            }
+        }
+
+        return s.size();
+    }
+};
