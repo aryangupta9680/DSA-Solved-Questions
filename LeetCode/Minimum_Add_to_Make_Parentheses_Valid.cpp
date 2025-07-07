07279b019868baec9339d369fb8b55d014c81bba
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-07-07 13:18:00 +0530

    Add solution for Minimum Add to Make Parentheses Valid problem

---
+++ LeetCode/Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int minAddToMakeValid(string s)
+    {
+        stack<int> st;
+        int count = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            // Opening bracket
+            if (s[i] == '(')
+            {
+                st.push(s[i]);
+            }
+
+            // Closing bracket
+            else
+            {
+                if (st.empty())
+                {
+                    count++;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+        return count + st.size();
+    }
+};
