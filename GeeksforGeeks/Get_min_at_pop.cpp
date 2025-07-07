836721e31fb04c0c8d232535eff450d28f28267d
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-07-07 15:38:42 +0530

    Add solution for Get min at pop problem

---
+++ GeeksforGeeks/Get_min_at_pop.cpp
@@ -0,0 +1,29 @@
+// Function to push all the elements into the stack.
+stack<int> _push(int arr[], int n)
+{
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            st.push(arr[i]);
+        }
+
+        else
+        {
+            st.push(min(arr[i], st.top()));
+        }
+    }
+
+    return st;
+}
+
+// Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int> s)
+{
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
