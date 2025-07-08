6fd3be1f6c9deb091360dce00fa8ea059016fa87
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-07-08 17:32:35 +0530

    Add solution for Next Greater Element problem

---
+++ CodingNinjas/Next_Greater_Element.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+vector<int> nextGreaterElement(vector<int> &arr, int n)
+{
+    vector<int> ans(n, -1);
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        while (!st.empty() && arr[st.top()] < arr[i])
+        {
+            ans[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    return ans;
+}
