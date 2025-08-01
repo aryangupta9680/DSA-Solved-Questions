ea251b62f952fe8db98bb7f7cd75add75319eedb
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-01 09:18:15 +0530

    Add second method

--- GeeksforGeeks/Size_of_Binary_Tree.cpp
+++ GeeksforGeeks/Size_of_Binary_Tree.cpp
@@ -1,5 +1,5 @@
+// Method 1
 /*
-
 Definition for Binary Tree Node
 struct Node
 {
@@ -34,5 +34,38 @@ public:
         int count = 0;
         Total(root, count);
         return count;
+    }
+};
+
+
+
+
+// Method 2:
+/*
+Definition for Binary Tree Node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    int getSize(Node *root)
+    {
+        if (root == NULL)
+        {
+            return 0;
+        }
+
+        return (1 + getSize(root->left) + getSize(root->right));
     }
 };
