51713ce11efc9c06b91d0fbc6274bf229cb4a1a7
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-01 09:13:17 +0530

    Add solution for Size of Binary Tree problem

---
+++ GeeksforGeeks/Size_of_Binary_Tree.cpp
@@ -0,0 +1,38 @@
+/*
+
+Definition for Binary Tree Node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    void Total(Node *root, int &count)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        count++;
+        Total(root->left, count);
+        Total(root->right, count);
+    }
+
+    int getSize(Node *root)
+    {
+        int count = 0;
+        Total(root, count);
+        return count;
+    }
+};
