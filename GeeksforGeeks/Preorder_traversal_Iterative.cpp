5fc440f154d0e9455fd27e16b5bc378541fd9f60
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-07 11:42:43 +0530

    Add second method

--- GeeksforGeeks/Preorder_traversal_Iterative.cpp
+++ GeeksforGeeks/Preorder_traversal_Iterative.cpp
@@ -1,3 +1,4 @@
+// Method 1:
 // User function Template for C++
 
 /* Tree Node
@@ -30,6 +31,66 @@ public:
             if (temp->left)
             {
                 s.push(temp->left);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+// Method 2: Using Morris Traversal
+// User function Template for C++
+
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+class Solution
+{
+public:
+    vector<int> preOrder(Node *root)
+    {
+        vector<int> ans;
+
+        while (root)
+        {
+            // Left subtree doesn't exist
+            if (!root->left)
+            {
+                ans.push_back(root->data);
+                root = root->right;
+            }
+
+            // Left subtree exists
+            else
+            {
+                Node *curr = root->left;
+                while (curr->right && curr->right != root)
+                {
+                    curr = curr->right;
+                }
+
+                // Left subtree not yet traversed
+                if (curr->right == NULL)
+                {
+                    ans.push_back(root->data);
+                    curr->right = root; // Make link to come back later
+                    root = root->left;
+                }
+
+                // Left subtree already traversed
+                else
+                {
+                    curr->right = NULL; // Remove the link
+                    root = root->right;
+                }
             }
         }
 
