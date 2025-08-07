aaa1a9589d17c7bbe7aa37e07a6a24cc66a67cc0
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-07 12:10:16 +0530

    Add second method

--- GeeksforGeeks/Iterative_Postorder.cpp
+++ GeeksforGeeks/Iterative_Postorder.cpp
@@ -1,3 +1,4 @@
+// Method 1:
 // User function Template for C++
 
 /* Tree Node
@@ -31,6 +32,66 @@ public:
             if (temp->right)
             {
                 s.push(temp->right);
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+
+
+
+// Method 2: Using Morris Traversal
+// User function Template for C++
+
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};*/
+class Solution
+{
+public:
+    vector<int> postOrder(Node *root)
+    {
+        vector<int> ans;
+
+        while (root)
+        {
+            // Right subtree doesn't exist
+            if (!root->right)
+            {
+                ans.push_back(root->data);
+                root = root->left;
+            }
+
+            // Right subtree exists
+            else
+            {
+                Node *curr = root->right;
+                while (curr->left && curr->left != root)
+                {
+                    curr = curr->left;
+                }
+
+                // Right subtree not yet traversed
+                if (curr->left == NULL)
+                {
+                    ans.push_back(root->data);
+                    curr->left = root; // Make link to come back later
+                    root = root->right;
+                }
+
+                // Right subtree already traversed
+                else
+                {
+                    curr->left = NULL; // Remove the link
+                    root = root->left;
+                }
             }
         }
 
