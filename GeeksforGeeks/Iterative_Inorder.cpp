8fe1b28a83618b5b4bb914d785f8c860fc125f3d
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-07 11:21:03 +0530

    Add second method

--- GeeksforGeeks/Iterative_Inorder.cpp
+++ GeeksforGeeks/Iterative_Inorder.cpp
@@ -1,3 +1,4 @@
+// Method 1:
 // User function Template for C++
 
 /* Tree Node
@@ -50,6 +51,64 @@ public:
             else
             {
                 ans.push_back(temp->data);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+// Method 2: Using Morris Traversal
+// User function Template for C++
+
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};*/
+class Solution
+{
+public:
+    vector<int> inOrder(Node *root)
+    {
+        vector<int> ans;
+
+        while (root)
+        {
+            // Left subtree doesn't exist
+            if (!root->left)
+            {
+                ans.push_back(root->data);
+                root = root->right;
+            }
+
+            // Left subtree exists
+            else
+            {
+                Node *curr = root->left;
+                while (curr->right && curr->right != root)
+                {
+                    curr = curr->right;
+                }
+
+                // Left subtree not yet traversed
+                if (curr->right == NULL)
+                {
+                    curr->right = root; // Make link to come back later
+                    root = root->left;
+                }
+
+                // Left subtree already traversed
+                else
+                {
+                    curr->right = NULL; // Remove the link
+                    ans.push_back(root->data);
+                    root = root->right;
+                }
             }
         }
 
