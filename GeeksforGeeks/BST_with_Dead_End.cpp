78f3a31291ad0482cf526b5b57adee512cfdc0ad
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-12 12:55:04 +0530

    Add solution for BST with Dead End problem

---
+++ GeeksforGeeks/BST_with_Dead_End.cpp
@@ -0,0 +1,46 @@
+/*The Node structure is
+class Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};*/
+
+class Solution
+{
+public:
+    bool Dead(Node *root, int lower, int upper)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+
+        // leaf node
+        if (!root->left && !root->right)
+        {
+            if (root->data - lower == 1 && upper - root->data == 1)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        return Dead(root->left, lower, root->data) || Dead(root->right, root->data, upper);
+    }
+
+    bool isDeadEnd(Node *root)
+    {
+        return Dead(root, 0, INT_MAX);
+    }
+};
