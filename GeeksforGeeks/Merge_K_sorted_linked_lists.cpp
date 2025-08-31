debaf4db19e7c74a84d18a23d7dd48614b798a19
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-08-31 18:54:21 +0530

    Add third method

--- GeeksforGeeks/Merge_K_sorted_linked_lists.cpp
+++ GeeksforGeeks/Merge_K_sorted_linked_lists.cpp
@@ -149,3 +149,67 @@ public:
         return arr[0];
     }
 };
+
+
+
+
+
+// Using Third Method:
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    class Compare
+    {
+    public:
+        bool operator()(Node *a, Node *b)
+        {
+            return a->data > b->data;
+        }
+    };
+
+    Node *mergeKLists(vector<Node *> &arr)
+    {
+        int k = arr.size();
+        // create Min Heap
+        priority_queue<Node *, vector<Node *>, Compare> p;
+
+        for (int i = 0; i < k; i++)
+        {
+            if (arr[i] != NULL)
+            {
+                p.push(arr[i]);
+            }
+        }
+
+        Node *root = new Node(0);
+        Node *tail = root;
+        Node *temp;
+
+        while (!p.empty())
+        {
+            temp = p.top();
+            p.pop();
+            tail->next = temp;
+            tail = tail->next;
+            if (temp->next)
+            {
+                p.push(temp->next);
+            }
+        }
+
+        return root->next;
+    }
+};
