1a2f990826c433bdbb1971d8330b200cec8b76a2
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-09-15 22:31:48 +0530

    Add second method

--- GeeksforGeeks/Undirected_Graph_Cycle.cpp
+++ GeeksforGeeks/Undirected_Graph_Cycle.cpp
@@ -54,3 +54,68 @@ public:
         return false;
     }
 };
+
+
+
+
+
+// Method 2: Using BFS
+class Solution
+{
+public:
+    bool BFS(int vertex, vector<vector<int>> &adj, vector<bool> &visited)
+    {
+        queue<pair<int, int>> q;
+        visited[vertex] = 1;
+        q.push(make_pair(vertex, -1)); // node, parent
+
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+
+            for (int j = 0; j < adj[node].size(); j++)
+            {
+                if (parent == adj[node][j])
+                {
+                    continue;
+                }
+
+                if (visited[adj[node][j]])
+                {
+                    return true;
+                }
+
+                visited[adj[node][j]] = 1;
+                q.push(make_pair(adj[node][j], node));
+            }
+        }
+
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>> &edges)
+    {
+        // Method 2: Using BFS
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i] && BFS(i, adj, visited))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
