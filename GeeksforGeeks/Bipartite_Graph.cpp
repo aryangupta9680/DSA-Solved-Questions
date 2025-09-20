10bf8e623de5e4f6d10966238f18ae1c0a18df24
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-09-20 21:24:18 +0530

    Add second method

--- GeeksforGeeks/Bipartite_Graph.cpp
+++ GeeksforGeeks/Bipartite_Graph.cpp
@@ -1,3 +1,4 @@
+// Method 1: Using BFS
 class Solution
 {
 public:
@@ -45,6 +46,74 @@ public:
                             }
                         }
                     }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+
+
+
+
+// Method 2: Using DFS
+class Solution
+{
+public:
+    bool checkBip(int node, vector<vector<int>> &adj, vector<int> &color)
+    {
+        // look at each neighbour
+        for (int j = 0; j < adj[node].size(); j++)
+        {
+            // color is not assigned to the neighbour
+            if (color[adj[node][j]] == -1)
+            {
+                color[adj[node][j]] = (color[node] + 1) % 2;
+
+                if (!checkBip(adj[node][j], adj, color))
+                {
+                    return false;
+                }
+            }
+
+            // color is assigned to the neighbour
+            else
+            {
+                if (color[node] == color[adj[node][j]])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool isBipartite(int V, vector<vector<int>> &edges)
+    {
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> color(V, -1);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1) // not assigned
+            {
+                color[i] = 0;
+                if (!checkBip(0, adj, color))
+                {
+                    return false;
                 }
             }
         }
