8c5d3b7bd3c10fead3bb5740b051f73c954f2251
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-09-20 19:23:56 +0530

    Add solution for Bipartite Graph problem

---
+++ GeeksforGeeks/Bipartite_Graph.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+public:
+    bool isBipartite(int V, vector<vector<int>> &edges)
+    {
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> color(V, -1);
+        queue<int> q;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                q.push(i);
+                color[i] = 0;
+
+                while (!q.empty())
+                {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int j = 0; j < adj[node].size(); j++)
+                    {
+                        // color not assigned to them
+                        if (color[adj[node][j]] == -1)
+                        {
+                            color[adj[node][j]] = (color[node] + 1) % 2;
+                            q.push(adj[node][j]);
+                        }
+
+                        // color is already assigned
+                        else
+                        {
+                            if (color[node] == color[adj[node][j]])
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
