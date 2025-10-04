40987f3599aed4d4533becf6198ff6753793ec02
aryangupta9680 <aryan.gupta9680@gmail.com>
2025-10-04 17:03:52 +0530

    Add solution for Parallel Courses III problem

---
+++ LeetCode/Parallel_Courses_III.cpp
@@ -0,0 +1,61 @@
+class Solution
+{
+public:
+    int minimumTime(int n, vector<vector<int>> &relations, vector<int> &time)
+    {
+        // create adjacency list
+        vector<int> adj[n];
+
+        for (int i = 0; i < relations.size(); i++)
+        {
+            // convert them into 0 based indexing
+            adj[relations[i][0] - 1].push_back(relations[i][1] - 1);
+        }
+
+        vector<int> InDeg(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < adj[i].size(); j++)
+            {
+                InDeg[adj[i][j]]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (!InDeg[i])
+            {
+                q.push(i);
+            }
+        }
+
+        // maximum time to complete previous course
+        vector<int> CourseTime(n, 0);
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            for (int j = 0; j < adj[node].size(); j++)
+            {
+                InDeg[adj[node][j]]--;
+                if (!InDeg[adj[node][j]])
+                {
+                    q.push(adj[node][j]);
+                }
+
+                // maximum time to complete previous course + present course time
+                CourseTime[adj[node][j]] = max(CourseTime[adj[node][j]], CourseTime[node] + time[node]);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(ans, CourseTime[i] + time[i]);
+        }
+
+        return ans;
+    }
+};
